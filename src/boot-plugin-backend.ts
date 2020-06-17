36382fdb0b6d2c9b583797c763856b58dbf614de
Esteban Victorio <esteban@octahedroid.com>
2020-06-17 11:07:21 -0700

    feat: change syntax to arrow functions

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -6,36 +6,34 @@ import * as theia from "@theia/plugin";
 
 // import { ContainersService } from "./containers-service";
 
-export function start(context: theia.PluginContext) {
+const start = (context: theia.PluginContext) => {
   console.log("Logging tasks:");
   theia.tasks.fetchTasks({ type: "che" }).then(
     (tasks) => {
-      console.log("Tasks:");
-      //   console.log(tasks);
-      tasks.map((task) => {
-        const { name } = task;
-        theia.window.showInformationMessage(`Task name: ${name}`);
-      });
+      for (const task of tasks) {
+        console.log(task.name);
+        console.log(task.definition);
+      }
     },
     (reason) => {
       console.error("No tasks :(");
       console.log(reason);
     }
   );
+};
 
-  //   const containersService = new ContainersService();
+//   const containersService = new ContainersService();
 
-  //   containersService.updateContainers().then(
-  //     () => {
-  //       // treeDataProvider.updateContainersTreeData(containersService.containers);
-  //       const containers = containersService.containers;
-  //       containers.forEach(({ commands }) => {});
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //       theia.window.showErrorMessage(error);
-  //     }
-  //   );
-}
-
-export function stop() {}
+//   containersService.updateContainers().then(
+//     () => {
+//       // treeDataProvider.updateContainersTreeData(containersService.containers);
+//       const containers = containersService.containers;
+//       containers.forEach(({ commands }) => {});
+//     },
+//     (error) => {
+//       console.error(error);
+//       theia.window.showErrorMessage(error);
+//     }
+//   );
+const stop = () => {};
+export { start, stop };
