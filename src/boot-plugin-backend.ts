eee62210780f1c207c5a70d8476d72b353a81cdc
Esteban Victorio <esteban@octahedroid.com>
2020-06-17 08:42:24 -0700

    feat: check task names

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -1,27 +1,25 @@
-
 /**
  * Generated using theia-plugin-generator
  */
 
-import * as theia from '@theia/plugin';
+import * as theia from "@theia/plugin";
 
-import { ContainersService } from './containers-service';
+// import { ContainersService } from "./containers-service";
 
-export function start(context: theia.PluginContext) {
-    const containersService = new ContainersService();
-    containersService.updateContainers().then(() => {
-        // treeDataProvider.updateContainersTreeData(containersService.containers);
-        const containers = containersService.containers;
-        containers.forEach(function (value) {
-            theia.window.showInformationMessage(value.name);
-        }); 
-    }, error => {
-        console.error(error);
-        theia.window.showErrorMessage(error);
-    });
-    console.log(theia.window.state);
+export async function start(context: theia.PluginContext) {
+  const tasks = await theia.tasks.fetchTasks({ type: "che" });
+  tasks.map(({ name }) => console.log(`Task name: ${name}`));
+  // const containersService = new ContainersService();
+
+  // containersService.updateContainers().then(() => {
+  //     // treeDataProvider.updateContainersTreeData(containersService.containers);
+  //     const containers = containersService.containers;
+  //     containers.forEach(({ commands,  }) => {
+  //     });
+  // }, error => {
+  //     console.error(error);
+  //     theia.window.showErrorMessage(error);
+  // });
 }
 
-export function stop() {
-
-}
+export function stop() {}
