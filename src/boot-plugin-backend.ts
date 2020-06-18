5ac2019527b50c2b6d2436d69d4e87afb0f14ec9
Omar Aguirre <omersguchigu@gmail.com>
2020-06-18 16:27:21 -0500

    fix: hardcoded functionality for develop mode

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -3,36 +3,35 @@
  */
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
-// import { ContainersService } from "./containers-service";
 const start = async (context: theia.PluginContext) => {
-  const wkspc = await che.workspace.getCurrentWorkspace();
-  const COMMAND = "init ... DEV che-theia";
-  const tasks = await theia.tasks.fetchTasks();
-  const { 0: initTask } = tasks.filter(
-    (task) => task.name === "init ... DEV che-theia"
-  );
-  if (initTask) {
-    const { 0: devfileCommand } = (wkspc.devfile &&
-      wkspc.devfile.commands &&
-      wkspc.devfile.commands.filter((command) => command.name === COMMAND)) || [
-      {},
-    ];
-    const componentAlias =
-      (devfileCommand.attributes &&
-        devfileCommand.attributes["componentAlias"]) ||
-      null;
-    console.log("Container retrieved ?", componentAlias ? "Yes" : "No");
-    console.log(componentAlias);
-    const target = {
-      containerName: componentAlias || "che-dev",
-      workingDir: "/projects/theia",
-    };
-    initTask.definition = (initTask.definition && {
-      ...initTask.definition,
-      target,
-    }) || { type: "" };
-    theia.tasks.executeTask(initTask);
-  }
+    const wkspc = await che.workspace.getCurrentWorkspace();
+    const COMMAND = "start develop mode";
+    const tasks = await theia.tasks.fetchTasks();
+    const { 0: initTask } = tasks.filter(
+        (task) => task.name === COMMAND
+    );
+    if (initTask) {
+        const { 0: devfileCommand } = (wkspc.devfile &&
+            wkspc.devfile.commands &&
+            wkspc.devfile.commands.filter((command) => command.name === COMMAND)) || [
+                {},
+            ];
+        const componentAlias =
+            (devfileCommand.attributes &&
+                devfileCommand.attributes["componentAlias"]) ||
+            null;
+        console.log("Container retrieved ?", componentAlias ? "Yes" : "No");
+        console.log(componentAlias);
+        const target = {
+            containerName: componentAlias || "nodejs",
+            workingDir: "/projects/gatsby-casper",
+        };
+        initTask.definition = (initTask.definition && {
+            ...initTask.definition,
+            target,
+        }) || { type: "" };
+        theia.tasks.executeTask(initTask);
+    }
 };
-const stop = () => {};
+const stop = () => { };
 export { start, stop };
