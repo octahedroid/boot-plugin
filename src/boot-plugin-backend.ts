cc7dd1b084f2f05f96d827c1a05b8101cd9a3814
Esteban Victorio <esteban@octahedroid.com>
2020-06-18 10:50:28 -0700

    feat: run task and reverted pulled branch to master on devfile

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -1,38 +1,38 @@
 /**
  * Generated using theia-plugin-generator
  */
-
 import * as theia from "@theia/plugin";
-
+import * as che from "@eclipse-che/plugin";
 // import { ContainersService } from "./containers-service";
-
-const start = (context: theia.PluginContext) => {
-  console.log("Logging tasks:");
-  theia.tasks.fetchTasks({ type: "che" }).then(
-    (tasks) => {
-      for (const task of tasks) {
-        theia.window.showInformationMessage("Task in ctx:", task.name);
-      }
-    },
-    (reason) => {
-      console.error("No tasks :(");
-      console.log(reason);
-    }
+const start = async (context: theia.PluginContext) => {
+  const wkspc = await che.workspace.getCurrentWorkspace();
+  const COMMAND = "init ... DEV che-theia";
+  const tasks = await theia.tasks.fetchTasks();
+  const { 0: initTask } = tasks.filter(
+    (task) => task.name === "init ... DEV che-theia"
   );
+  if (initTask) {
+    const { 0: devfileCommand } = (wkspc.devfile &&
+      wkspc.devfile.commands &&
+      wkspc.devfile.commands.filter((command) => command.name === COMMAND)) || [
+      {},
+    ];
+    const componentAlias =
+      (devfileCommand.attributes &&
+        devfileCommand.attributes["componentAlias"]) ||
+      null;
+    console.log("Container retrieved ?", componentAlias ? "Yes" : "No");
+    console.log(componentAlias);
+    const target = {
+      containerName: componentAlias || "che-dev",
+      workingDir: "/projects/theia",
+    };
+    initTask.definition = (initTask.definition && {
+      ...initTask.definition,
+      target,
+    }) || { type: "" };
+    theia.tasks.executeTask(initTask);
+  }
 };
-
-//   const containersService = new ContainersService();
-
-//   containersService.updateContainers().then(
-//     () => {
-//       // treeDataProvider.updateContainersTreeData(containersService.containers);
-//       const containers = containersService.containers;
-//       containers.forEach(({ commands }) => {});
-//     },
-//     (error) => {
-//       console.error(error);
-//       theia.window.showErrorMessage(error);
-//     }
-//   );
 const stop = () => {};
 export { start, stop };
