2c8f898b264c6cc35c4fe093f3117c010bbddfc0
Omar Aguirre <omersguchigu@gmail.com>
2020-06-22 17:18:58 -0500

    refactor: call star develop mode command and change gatsby for official starter

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -6,32 +6,9 @@ import * as che from "@eclipse-che/plugin";
 const start = async (context: theia.PluginContext) => {
     const wkspc = await che.workspace.getCurrentWorkspace();
     const COMMAND = "start develop mode";
-    const tasks = await theia.tasks.fetchTasks();
-    const { 0: initTask } = tasks.filter(
-        (task) => task.name === COMMAND
-    );
-    if (initTask) {
-        const { 0: devfileCommand } = (wkspc.devfile &&
-            wkspc.devfile.commands &&
-            wkspc.devfile.commands.filter((command) => command.name === COMMAND)) || [
-                {},
-            ];
-        const componentAlias =
-            (devfileCommand.attributes &&
-                devfileCommand.attributes["componentAlias"]) ||
-            null;
-        console.log("Container retrieved ?", componentAlias ? "Yes" : "No");
-        console.log(componentAlias);
-        const target = {
-            containerName: componentAlias || "nodejs",
-            workingDir: "/projects/gatsby-casper",
-        };
-        initTask.definition = (initTask.definition && {
-            ...initTask.definition,
-            target,
-        }) || { type: "" };
-        theia.tasks.executeTask(initTask);
-    }
+    const tasks = await theia.tasks.fetchTasks({ type: 'che' });
+    const { 0: initTask } = tasks.filter(({ name }: theia.Task) => name === COMMAND)
+    initTask && theia.tasks.executeTask(initTask)
 };
 const stop = () => { };
 export { start, stop };
