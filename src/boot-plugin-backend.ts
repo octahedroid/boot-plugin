142352e938393edb27eb11eab4b4cec04f12f75f
Omar Aguirre <omersguchigu@gmail.com>
2020-06-22 18:49:47 -0500

    feat: wrapping getCurrentWorkspace on a promise

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -4,11 +4,13 @@
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
 const start = async (context: theia.PluginContext) => {
-    const wkspc = await che.workspace.getCurrentWorkspace();
-    const COMMAND = "start develop mode";
-    const tasks = await theia.tasks.fetchTasks({ type: 'che' });
-    const { 0: initTask } = tasks.filter(({ name }: theia.Task) => name === COMMAND)
-    initTask && theia.tasks.executeTask(initTask)
+    await che.workspace.getCurrentWorkspace().then(async () => {
+        const COMMAND = "start develop mode";
+        const tasks = await theia.tasks.fetchTasks({ type: 'che' });
+        const { 0: initTask } = tasks.filter(({ name }: theia.Task) => name === COMMAND)
+        initTask && theia.tasks.executeTask(initTask)
+    });
+
 };
 const stop = () => { };
 export { start, stop };
