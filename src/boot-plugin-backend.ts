bf42a8aa59dbb97f59033a109b07e1f62f71e1db
Esteban Victorio <esteban@octahedroid.com>
2020-06-24 14:54:14 -0700

    feat: turn onto che task

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -6,22 +6,24 @@ import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
 import { bootTasks } from "./tasks";
 
-const handleEndTask = async (event: theia.TaskEndEvent) => {
-  const { execution } = event;
-  const { task } = execution;
-  const { name } = task;
+const handleEndTask = async (event: che.TaskExitedEvent) => {
+  // const { execution } = event;
+  // const { task } = execution;
+  // const { name } = task;
 
-  switch (name) {
-    case "install":
-      await theia.tasks.executeTask(bootTasks.develop);
-      break;
-    case "develop":
-      console.log("Develop task ended!!!");
-      break;
-  }
+  // switch (name) {
+  //   case "install":
+  //     await theia.tasks.executeTask(bootTasks.develop);
+  //     break;
+  //   case "develop":
+  //     console.log("Develop task ended!!!");
+  //     break;
+  // }
+  console.log("Task Exited Event:");
+  console.log(event);
 };
 
-theia.tasks.onDidEndTask(handleEndTask);
+che.task.onDidEndTask(handleEndTask);
 
 const start = async (context: theia.PluginContext) => {
   await che.workspace.getCurrentWorkspace();
