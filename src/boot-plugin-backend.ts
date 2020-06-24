3c1c5137ba1621aa4cdeedd76dfc00447382f2f9
Esteban Victorio <esteban@octahedroid.com>
2020-06-24 13:18:20 -0700

    feat: add boot tasks as object

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -5,10 +5,12 @@
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
 
-const start = async (context: theia.PluginContext) => {
-  await che.workspace.getCurrentWorkspace();
-  const initTask: theia.Task = {
-    name: "Install",
+const bootTasks: {
+  install: theia.Task;
+  develop: theia.Task;
+} = {
+  install: {
+    name: "install",
     scope: theia.TaskScope.Workspace,
     definition: {
       type: "che",
@@ -20,13 +22,47 @@ const start = async (context: theia.PluginContext) => {
     },
     execution: {
       command: "yarn",
-      args: ["&& yarn develop"],
       options: {
         cwd: "/projects/frontend",
       },
     },
-  };
-  await theia.tasks.executeTask(initTask);
+  },
+  develop: {
+    name: "develop",
+    scope: theia.TaskScope.Workspace,
+    definition: {
+      type: "che",
+      target: {
+        component: "nodejs",
+        containerName: "nodejs",
+        workingDir: "/projects/frontend",
+      },
+    },
+    execution: {
+      command: "yarn",
+      args: ["develop"],
+      options: {
+        cwd: "/projects/frontend",
+      },
+    },
+  },
+};
+
+const handleEndTask = async (event: theia.TaskEndEvent) => {
+  const { execution } = event;
+  const { task } = execution;
+  const { name } = task;
+
+  if (name === "install") {
+    await theia.tasks.executeTask(bootTasks.develop);
+  }
+};
+
+theia.tasks.onDidEndTask(handleEndTask);
+
+const start = async (context: theia.PluginContext) => {
+  await che.workspace.getCurrentWorkspace();
+  await theia.tasks.executeTask(bootTasks.install);
 };
 function stop() {}
 export { start, stop };
