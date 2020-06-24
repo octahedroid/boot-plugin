e5912a8c4f885a2731f80a2c722d9a3092b44ac2
Esteban Victorio <esteban@octahedroid.com>
2020-06-24 10:32:47 -0700

    feat: task with new definition

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -5,42 +5,22 @@
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
 const SHELL_TASK_TYPE = "shell";
-theia.tasks.onDidEndTask(async (e: theia.TaskEndEvent) => {
-  const { execution } = e;
-  const { task } = execution;
-  const { name } = task;
-  if (name === "Yarn install chingado") {
-    console.log("Exec:");
-    console.log(execution);
-  }
-});
-
 const start = async (context: theia.PluginContext) => {
   await che.workspace.getCurrentWorkspace();
   const initTask: theia.Task = {
     definition: {
       type: "che",
-      label: "Yarn install chingado",
+      label: "Yarn install",
       command: "yarn && yarn develop",
       target: {
         workspaceId: "",
         component: "nodejs",
         workingDir: "/projects/frontend",
       },
-      cwd: "/projects/frontend",
       previewUrl: "",
     },
-    execution: {
-      cwd: "/projects/frontend",
-      options: {
-        cwd: "/projects/frontend",
-      },
-    },
   };
-
   await theia.tasks.executeTask(initTask);
 };
-
 function stop() {}
-
 export { start, stop };
