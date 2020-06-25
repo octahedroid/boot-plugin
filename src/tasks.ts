7abae47fe4d798cb82ad40b5df6a55e540b9971e
Esteban Victorio <esteban@octahedroid.com>
2020-06-25 15:03:33 -0700

    feat: join flags

--- src/tasks.ts
+++ src/tasks.ts
@@ -22,7 +22,7 @@ const tasks: {
     },
     execution: {
       command: "sh",
-      args: ["-v", "-c", "'yarn && yarn develop'"],
+      args: ["-v -c", "'yarn && yarn develop'"],
       options: {
         cwd: "/projects/frontend",
       },
