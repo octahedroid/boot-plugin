4d5cdb8d2db0ece8fb8667b0f15b80b30fb4485b
Esteban Victorio <estebanvictorio92@gmail.com>
2020-06-30 15:59:21 -0700

    feat: change container name

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,33 +1,33 @@
 import { Task, TaskScope } from "@theia/plugin";
 
 const tasks: {
-    bootstrap: Task;
+  bootstrap: Task;
 } = {
-    bootstrap: {
-        name: "install",
-        scope: TaskScope.Global,
-        definition: {
-            type: "che",
-            config: {
-                label: "install",
-                preview: {
-                    notifications: "off",
-                },
-            },
-            target: {
-                component: "che-dev",
-                containerName: "che-dev",
-                workingDir: "/projects/frontend",
-            },
+  bootstrap: {
+    name: "install",
+    scope: TaskScope.Global,
+    definition: {
+      type: "che",
+      config: {
+        label: "install",
+        preview: {
+          notifications: "off",
         },
-        execution: {
-            command: "bash",
-            options: {
-                cwd: "/projects/frontend",
-                shellArgs: ["-c", "'yarn && yarn develop'"],
-            },
-        },
+      },
+      target: {
+        component: "nodejs",
+        containerName: "nodejs",
+        workingDir: "/projects/frontend",
+      },
     },
+    execution: {
+      command: "bash",
+      options: {
+        cwd: "/projects/frontend",
+        shellArgs: ["-c", "'yarn && yarn develop'"],
+      },
+    },
+  },
 };
 
 export { tasks };
