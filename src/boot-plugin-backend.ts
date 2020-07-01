088afde63fa67173ace7f0498b99815fe5ebba2d
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 13:19:48 -0700

    feat: remove await from promise to let it run until 30 secs have passed

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -6,11 +6,20 @@ import { tasks } from "./tasks";
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
 
-const start = async (context: theia.PluginContext) => {
-  await new Promise((resolve) => setTimeout(resolve, 30000));
-  const workspace = await che.workspace.getCurrentWorkspace();
-  const { bootstrap } = tasks;
-  await theia.tasks.executeTask(bootstrap);
+const runDelayedBootstrap = (resolve) => {
+  setTimeout(async () => {
+    const workspace = await che.workspace.getCurrentWorkspace();
+    const { bootstrap } = tasks;
+    await theia.tasks.executeTask(bootstrap);
+    console.log("Terminated tasks, resolving promise");
+    resolve();
+  }, 30000);
 };
+
+const start = async (context: theia.PluginContext) => {
+  new Promise(runDelayedBootstrap);
+};
+
 function stop() {}
+
 export { start, stop };
