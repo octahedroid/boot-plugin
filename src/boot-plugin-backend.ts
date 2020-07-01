af68bbf91d1b8f1311a62d7a6ad9df2bc1ec5927
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 13:09:49 -0700

    feat: add timeout

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -2,34 +2,15 @@
  * Generated using theia-plugin-generator
  */
 
-import * as path from "path";
 import { tasks } from "./tasks";
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
-import WorkspaceClient from "@eclipse-che/workspace-client";
-// const workspace = await che.workspace.getCurrentWorkspace();
-//     const { bootstrap } = tasks;
-//     await theia.tasks.executeTask(bootstrap);
 
 const start = async (context: theia.PluginContext) => {
-  let status = null;
-  let clientId = null;
-  const entryPoint = "/api/workspace";
+  await new Promise((resolve) => setTimeout(resolve, 30000));
   const workspace = await che.workspace.getCurrentWorkspace();
-  const masterApiClient = WorkspaceClient.getJsonRpcApi(entryPoint);
-  const connectionPromise = masterApiClient.connect(entryPoint);
-  await connectionPromise.then(() => {
-    clientId = masterApiClient.getClientId();
-  });
-
-  const handler = (info) => {
-    console.table(info);
-  };
-  if (workspace.id) {
-    masterApiClient.subscribeWorkspaceStatus(workspace.id, handler);
-  } else {
-    console.log("No wkspc id");
-  }
+  const { bootstrap } = tasks;
+  await theia.tasks.executeTask(bootstrap);
 };
 function stop() {}
 export { start, stop };
