b38ec67127ead32e90257bd9f5ed025721629a06
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 13:31:11 -0700

    feat: change release tag

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -7,19 +7,19 @@ import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
 
 const runDelayedBootstrap = (resolve: any) => {
-  setTimeout(async () => {
-    await che.workspace.getCurrentWorkspace();
-    const { bootstrap } = tasks;
-    await theia.tasks.executeTask(bootstrap);
-    console.log("Terminated tasks, resolving promise");
-    resolve();
-  }, 30000);
+    setTimeout(async () => {
+        await che.workspace.getCurrentWorkspace();
+        const { bootstrap } = tasks;
+        await theia.tasks.executeTask(bootstrap);
+        console.log("Terminated tasks, resolving promise");
+        resolve();
+    }, 30000);
 };
 
 const start = async (context: theia.PluginContext) => {
-  new Promise(runDelayedBootstrap);
+    new Promise(runDelayedBootstrap);
 };
 
-function stop() {}
+function stop() { }
 
 export { start, stop };
