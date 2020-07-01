928fa52774f63b874d3ee8b9c4ca18695ed22ec7
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 14:46:03 -0700

    feat: change release

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,30 +1,30 @@
 import { Task, TaskScope } from "@theia/plugin";
 
 const tasks: {
-    bootstrap: Task;
+  bootstrap: Task;
 } = {
-    bootstrap: {
-        name: "dev",
-        scope: TaskScope.Workspace,
-        definition: {
-            type: "shell",
-            config: {
-                label: "dev",
-            },
-            target: {
-                component: "nodejs",
-                containerName: "nodejs",
-                workingDir: "/projects/frontend",
-            },
-        },
-        execution: {
-            command: "sh",
-            args: ["-c", "'npm install && npm run develop'"],
-            options: {
-                cwd: "/projects/frontend",
-            },
-        },
+  bootstrap: {
+    name: "dev",
+    scope: TaskScope.Workspace,
+    definition: {
+      type: "shell",
+      config: {
+        label: "dev",
+      },
+      target: {
+        component: "nodejs",
+        containerName: "nodejs",
+        workingDir: "/projects/frontend",
+      },
     },
+    execution: {
+      command: "sh",
+      args: ["-c", "'yarn install --frozen-lockfile && yarn develop'"],
+      options: {
+        cwd: "/projects/frontend",
+      },
+    },
+  },
 };
 
 export { tasks };
