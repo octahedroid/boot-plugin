240cdee5b15c6ca5264ffc48e2c99381926b3413
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 11:03:44 -0700

    feat: watch for dir changes

--- src/boot-plugin-backend.ts
+++ src/boot-plugin-backend.ts
@@ -2,14 +2,24 @@
  * Generated using theia-plugin-generator
  */
 
+import * as path from "path";
+import { tasks } from "./tasks";
+import * as chokidar from "chokidar";
 import * as theia from "@theia/plugin";
 import * as che from "@eclipse-che/plugin";
-import { tasks } from "./tasks";
 
-const start = async (context: theia.PluginContext) => {
+const installEffect = async (path: string) => {
+  if (/\/frontend\/package.json/.test(path)) {
     await che.workspace.getCurrentWorkspace();
     const { bootstrap } = tasks;
     await theia.tasks.executeTask(bootstrap);
+  }
 };
-function stop() { }
+
+const start = async (context: theia.PluginContext) => {
+  const cwd = path.resolve(__dirname);
+  const watcher = chokidar.watch(cwd);
+  watcher.on("add", installEffect);
+};
+function stop() {}
 export { start, stop };
