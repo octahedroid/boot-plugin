b62bab60ae862e43897e9e0e09e98a2bc1fe10de
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 21:11:08 -0700

    feat: change command order

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,33 +1,35 @@
 import { Task, TaskScope } from "@theia/plugin";
 
 const tasks: {
-    bootstrap: Task;
+  bootstrap: Task;
 } = {
-    bootstrap: {
-        name: "dev",
-        scope: TaskScope.Workspace,
-        definition: {
-            type: "shell",
-            config: {
-                label: "dev",
-            },
-            target: {
-                component: "nodejs",
-                containerName: "nodejs",
-                workingDir: "/projects/frontend",
-            },
-        },
-        execution: {
-            command: "sh",
-            args: [
-                "-c",
-                "'yarn install --frozen-lockfile && yarn develop' > logs.txt",
-            ],
-            options: {
-                cwd: "/projects/frontend",
-            },
-        },
+  bootstrap: {
+    name: "dev",
+    scope: TaskScope.Workspace,
+    definition: {
+      type: "shell",
+      config: {
+        label: "dev",
+      },
+      target: {
+        component: "nodejs",
+        containerName: "nodejs",
+        workingDir: "/projects/frontend",
+      },
     },
+    execution: {
+      command: "sh",
+      args: [
+        ">",
+        "logs.txt",
+        "-c",
+        "'yarn install --frozen-lockfile && yarn develop'",
+      ],
+      options: {
+        cwd: "/projects/frontend",
+      },
+    },
+  },
 };
 
 export { tasks };
