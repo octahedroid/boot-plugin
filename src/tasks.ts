1021f676a742c3c04e13999c3cbe4fe8d557c915
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 20:38:01 -0700

    feat: add logging to file

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,30 +1,34 @@
 import { Task, TaskScope } from "@theia/plugin";
 
 const tasks: {
-    bootstrap: Task;
+  bootstrap: Task;
+  resultCheck: Task;
 } = {
-    bootstrap: {
-        name: "dev",
-        scope: TaskScope.Workspace,
-        definition: {
-            type: "shell",
-            config: {
-                label: "dev",
-            },
-            target: {
-                component: "nodejs",
-                containerName: "nodejs",
-                workingDir: "/projects/frontend",
-            },
-        },
-        execution: {
-            command: "sh",
-            args: ["-c", "'yarn install --frozen-lockfile && yarn develop'"],
-            options: {
-                cwd: "/projects/frontend",
-            },
-        },
+  bootstrap: {
+    name: "dev",
+    scope: TaskScope.Workspace,
+    definition: {
+      type: "shell",
+      config: {
+        label: "dev",
+      },
+      target: {
+        component: "nodejs",
+        containerName: "nodejs",
+        workingDir: "/projects/frontend",
+      },
     },
+    execution: {
+      command: "sh",
+      args: [
+        "-c",
+        "'yarn install --frozen-lockfile && yarn develop' > process.log",
+      ],
+      options: {
+        cwd: "/projects/frontend",
+      },
+    },
+  },
 };
 
 export { tasks };
