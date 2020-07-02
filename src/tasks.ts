728dc84913ef30c461959b804fbbdcc5444298d3
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 21:06:37 -0700

    fix: format and change meta version

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,33 +1,33 @@
 import { Task, TaskScope } from "@theia/plugin";
 
 const tasks: {
-  bootstrap: Task;
+    bootstrap: Task;
 } = {
-  bootstrap: {
-    name: "dev",
-    scope: TaskScope.Workspace,
-    definition: {
-      type: "shell",
-      config: {
-        label: "dev",
-      },
-      target: {
-        component: "nodejs",
-        containerName: "nodejs",
-        workingDir: "/projects/frontend",
-      },
+    bootstrap: {
+        name: "dev",
+        scope: TaskScope.Workspace,
+        definition: {
+            type: "shell",
+            config: {
+                label: "dev",
+            },
+            target: {
+                component: "nodejs",
+                containerName: "nodejs",
+                workingDir: "/projects/frontend",
+            },
+        },
+        execution: {
+            command: "sh",
+            args: [
+                "-c",
+                "'yarn install --frozen-lockfile && yarn develop' > logs.txt",
+            ],
+            options: {
+                cwd: "/projects/frontend",
+            },
+        },
     },
-    execution: {
-      command: "sh",
-      args: [
-        "-c",
-        "'yarn install --frozen-lockfile && yarn develop' > logs.txt",
-      ],
-      options: {
-        cwd: "/projects/frontend",
-      },
-    },
-  },
 };
 
 export { tasks };
