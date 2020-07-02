19ea2f82ff4318ac87c2881636f7f93c927e4ae6
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-01 22:01:50 -0700

    feat: change memory limit and fix task name in event handler

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,53 +1,53 @@
 import { Task, TaskScope } from "@theia/plugin";
 
 const tasks: {
-  install: Task;
-  preview: Task;
+    install: Task;
+    preview: Task;
 } = {
-  install: {
-    name: "yarn-install",
-    scope: TaskScope.Workspace,
-    definition: {
-      type: "shell",
-      config: {
-        label: "yarn-install",
-      },
-      target: {
-        component: "nodejs",
-        containerName: "nodejs",
-        workingDir: "/projects/frontend",
-      },
+    install: {
+        name: "yarn-install",
+        scope: TaskScope.Workspace,
+        definition: {
+            type: "shell",
+            config: {
+                label: "yarn-install",
+            },
+            target: {
+                component: "nodejs",
+                containerName: "nodejs",
+                workingDir: "/projects/frontend",
+            },
+        },
+        execution: {
+            command: "yarn",
+            args: [">", "install-log.txt", "--verbose"],
+            options: {
+                cwd: "/projects/frontend",
+            },
+        },
     },
-    execution: {
-      command: "yarn",
-      args: [">", "install-log.txt", "--verbose"],
-      options: {
-        cwd: "/projects/frontend",
-      },
+    preview: {
+        name: "gatsby-preview",
+        scope: TaskScope.Workspace,
+        definition: {
+            type: "shell",
+            config: {
+                label: "gatsby-preview",
+            },
+            target: {
+                component: "nodejs",
+                containerName: "nodejs",
+                workingDir: "/projects/frontend",
+            },
+        },
+        execution: {
+            command: "yarn",
+            args: [">", "preview-log.txt", "develop", "--verbose"],
+            options: {
+                cwd: "/projects/frontend",
+            },
+        },
     },
-  },
-  preview: {
-    name: "gatsby-preview",
-    scope: TaskScope.Workspace,
-    definition: {
-      type: "shell",
-      config: {
-        label: "gatsby-preview",
-      },
-      target: {
-        component: "nodejs",
-        containerName: "nodejs",
-        workingDir: "/projects/frontend",
-      },
-    },
-    execution: {
-      command: "yarn",
-      args: [">", "preview-log.txt", "develop", "--verbose"],
-      options: {
-        cwd: "/projects/frontend",
-      },
-    },
-  },
 };
 
 export { tasks };
