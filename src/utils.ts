d985005f68649f778b3adcdf45d0a0f35c4c5e68
Esteban Victorio <estebanvictorio92@gmail.com>
2020-07-06 10:27:01 -0700

    feat: print selected machine and servers

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,25 @@ interface CheMachine {
   name: string;
 }
 
+const MACHINE = "nodejs";
+
+const getMachineServers = (machine: che.workspace.Machine) => {
+  if (!machine) {
+    return {};
+  }
+
+  const { servers } = machine;
+  console.table(servers);
+};
+
+const bySelectedMachine = (selectedMachineName: string, machines: any) => {
+  const { 0: machineID } = Object.keys(machines).filter(
+    (machineName) => machineName === selectedMachineName
+  );
+  const { [machineID]: selectedMachine } = machines || {};
+  return selectedMachine;
+};
+
 const getPreviewUrl = async (workspace: che.workspace.Workspace) => {
   if (!workspace || !workspace.id || !workspace.runtime) {
     return "";
@@ -12,10 +31,15 @@ const getPreviewUrl = async (workspace: che.workspace.Workspace) => {
   const { id } = workspace;
 
   const { machines } = workspace.runtime! || { machines: {} };
+  const selectedMachine = bySelectedMachine(MACHINE, machines);
 
   console.log(`Workspace: ${id}`);
   console.log("Machines:");
   console.table(machines);
+  console.log("Selected Machine:");
+  console.log(selectedMachine);
+  console.log("Servers:");
+  getMachineServers(selectedMachine);
 };
 
 export { getPreviewUrl };
