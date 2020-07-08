6980302039b0623346ad2f4f52d25c196f2d7b69
Omar Aguirre <omersguchigu@gmail.com>
2020-07-07 20:18:09 -0500

    feat: formatting code

--- src/utils.ts
+++ src/utils.ts
@@ -7,108 +7,108 @@ import * as chePlugin from "@eclipse-che/plugin";
 import { WorkspacePort } from "./workspace-port";
 
 interface CheMachine {
-  props: che.workspace.Machine;
-  id: string;
+    props: che.workspace.Machine;
+    id: string;
 }
 
 interface CheMachineServer {
-  name: string;
-  props: che.workspace.Server;
+    name: string;
+    props: che.workspace.Server;
 }
 
 let currentTimeout: NodeJS.Timeout | null = null;
 
 const getWorkspacePorts = (
-  workspace: che.workspace.Workspace
+    workspace: che.workspace.Workspace
 ): WorkspacePort[] => {
-  const ports: WorkspacePort[] = [];
-  if (!workspace) {
-    return ports;
-  }
+    const ports: WorkspacePort[] = [];
+    if (!workspace) {
+        return ports;
+    }
 
-  if (!workspace.id) {
-    return ports;
-  }
+    if (!workspace.id) {
+        return ports;
+    }
 
-  if (!workspace.runtime) {
-    return ports;
-  }
+    if (!workspace.runtime) {
+        return ports;
+    }
 
-  if (!workspace.runtime!.machines) {
-    return ports;
-  }
+    if (!workspace.runtime!.machines) {
+        return ports;
+    }
 
-  const machines = workspace.runtime!.machines!;
+    const machines = workspace.runtime!.machines!;
 
-  const machinesById: CheMachine[] =
-    Object.keys(machines).map((id) => ({
-      id,
-      props: machines[id],
-    })) || [];
+    const machinesById: CheMachine[] =
+        Object.keys(machines).map((id) => ({
+            id,
+            props: machines[id],
+        })) || [];
 
-  machinesById.map((machine: CheMachine) => {
-    const servers = machine.props.servers || {};
-    const serversByName: CheMachineServer[] = Object.keys(servers).map(
-      (name) => ({
-        name,
-        props: servers[name],
-      })
-    );
+    machinesById.map((machine: CheMachine) => {
+        const servers = machine.props.servers || {};
+        const serversByName: CheMachineServer[] = Object.keys(servers).map(
+            (name) => ({
+                name,
+                props: servers[name],
+            })
+        );
 
-    serversByName.map(({ name: serverName, props }: CheMachineServer) => {
-      const url = props.url!;
-      const attributes = props.attributes!;
-      const portNumber = attributes.port!;
-      ports.push({ url, portNumber, serverName });
+        serversByName.map(({ name: serverName, props }: CheMachineServer) => {
+            const url = props.url!;
+            const attributes = props.attributes!;
+            const portNumber = attributes.port!;
+            ports.push({ url, portNumber, serverName });
+        });
     });
-  });
 
-  return ports;
+    return ports;
 };
 
 const pollingEffect = async (url: string) => {
-  const urlOptions = parse(url);
-  const options = {
-    ...urlOptions,
-    rejectUnauthorized: false,
-    agent: new https.Agent({
-      rejectUnauthorized: false,
-    }),
-  };
+    const urlOptions = parse(url);
+    const options = {
+        ...urlOptions,
+        rejectUnauthorized: false,
+        agent: new https.Agent({
+            rejectUnauthorized: false,
+        }),
+    };
 
-  https.get(options, (res: http.IncomingMessage) => {
-    res.on("data", () => {
-      const { statusCode: status } = res;
-      status !== 200 &&
-        (currentTimeout = setTimeout(() => pollingEffect(url), 0));
-      status !== 200 && console.log(`Request finished on a :${status}`);
-      status === 200 && console.log("URL can now be previewed!:", url);
+    https.get(options, (res: http.IncomingMessage) => {
+        res.on("data", () => {
+            const { statusCode: status } = res;
+            status !== 200 &&
+                (currentTimeout = setTimeout(() => pollingEffect(url), 0));
+            status !== 200 && console.log(`Request finished on a :${status}`);
+            status === 200 && console.log("URL can now be previewed!:", url);
+        });
     });
-  });
 };
 
 const logPort = async (port: Port) => {
-  console.log(
-    `Port exposed: ${port.portNumber}, Interface: ${port.interfaceListen}`
-  );
+    console.log(
+        `Port exposed: ${port.portNumber}, Interface: ${port.interfaceListen}`
+    );
 
-  const workspace = await chePlugin.workspace.getCurrentWorkspace();
-  const { 0: serverPort } = getWorkspacePorts(workspace).filter(
-    (workspacePort) =>
-      parseInt(workspacePort.portNumber, 10) === port.portNumber &&
-      workspacePort.serverName === "nodejs"
-  );
+    const workspace = await chePlugin.workspace.getCurrentWorkspace();
+    const { 0: serverPort } = getWorkspacePorts(workspace).filter(
+        (workspacePort) =>
+            parseInt(workspacePort.portNumber, 10) === port.portNumber &&
+            workspacePort.serverName === "nodejs"
+    );
 
-  if (serverPort && serverPort.url) {
-    const { url } = serverPort;
-    currentTimeout = setTimeout(() => pollingEffect(url), 0);
-  }
+    if (serverPort && serverPort.url) {
+        const { url } = serverPort;
+        currentTimeout = setTimeout(() => pollingEffect(url), 0);
+    }
 };
 
 const handleOpenPort = async (port: Port) => {
-  console.log("Logging opened port");
-  console.log("Interface listen:");
-  await logPort(port);
+    console.log("Logging opened port");
+    console.log("Interface listen:");
+    await logPort(port);
 };
 
 export { logPort, handleOpenPort, getWorkspacePorts };
